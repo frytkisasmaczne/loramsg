b8ae7df3fbf25ad69b47d5871242e0ddde34771d
frytkisasmaczne <frytkisasmaczne@gmail.com>
2023-10-17 12:21:17 +0200

    builds but crashes

--- app/src/main/java/pl/denpa/loramsg2/MainActivity.kt
+++ app/src/main/java/pl/denpa/loramsg2/MainActivity.kt
@@ -17,77 +17,72 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
-import com.hoho.android.usbserial.driver.SerialTimeoutException
-import com.hoho.android.usbserial.driver.UsbSerialPort
-import com.hoho.android.usbserial.driver.UsbSerialProber
-import com.hoho.android.usbserial.util.SerialInputOutputManager
 
-
-class MainActivity : AppCompatActivity(), SerialInputOutputManager.Listener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val boardConnector = BoardConnector(this, intent.getParcelableExtra(UsbManager.EXTRA_DEVICE))
         setContentView(R.layout.activity_main)
     }
 
-    val boardConnector = BoardConnector(this, intent.getParcelableExtra(UsbManager.EXTRA_DEVICE))
-
-    fun init() {
-
-        permissionIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
-        val filter = IntentFilter(ACTION_USB_PERMISSION)
-        registerReceiver(usbReceiver, filter)
-        manager.requestPermission(device, permissionIntent)
 
 
-    }
-
-    fun connect() {
-        if (::port.isInitialized) {
-            if (port.isOpen) {
-                Toast.makeText(applicationContext, "port already opened", Toast.LENGTH_SHORT).show()
-                return
-            }
-        }
-
-        val availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager)
-        if (availableDrivers.isEmpty()) {
-            Toast.makeText(applicationContext, "no driver found", Toast.LENGTH_SHORT).show()
-            return;
-        }
-
-        val driver = availableDrivers[0]
-        val connection = manager.openDevice(driver.device)
-        if (connection == null) {
-            Toast.makeText(applicationContext, "can't open device", Toast.LENGTH_SHORT).show()
-            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
-            return
-        }
-
-        port = driver.ports[0] // Most devices have just one port (port 0)
-
-        port.open(connection)
-        port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
-
-        SerialInputOutputManager(port, this).start()
-//        Toast.makeText(applicationContext, "opened port POGCHAMP", Toast.LENGTH_SHORT).show()
-    }
-
-    fun sendbs(v: View) {
-        if (::port.isInitialized) {
-            if (port.isOpen) {
-                try {
-                    val edittext = findViewById<EditText>(R.id.edittext)
-                    port.write((edittext.text.toString()+"\n").encodeToByteArray(), WRITE_WAIT_MILLIS)
-                } catch (e: SerialTimeoutException) {
-                    Toast.makeText(applicationContext, "SerialTimeoutException: $e", Toast.LENGTH_SHORT).show()
-                }
-
-                return
-            }
-        }
-        Toast.makeText(applicationContext, "port not open", Toast.LENGTH_SHORT).show()
-    }
+//    fun init() {
+//
+//        permissionIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+//        val filter = IntentFilter(ACTION_USB_PERMISSION)
+//        registerReceiver(usbReceiver, filter)
+//        manager.requestPermission(device, permissionIntent)
+//
+//
+//    }
+//
+//    fun connect() {
+//        if (::port.isInitialized) {
+//            if (port.isOpen) {
+//                Toast.makeText(applicationContext, "port already opened", Toast.LENGTH_SHORT).show()
+//                return
+//            }
+//        }
+//
+//        val availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager)
+//        if (availableDrivers.isEmpty()) {
+//            Toast.makeText(applicationContext, "no driver found", Toast.LENGTH_SHORT).show()
+//            return;
+//        }
+//
+//        val driver = availableDrivers[0]
+//        val connection = manager.openDevice(driver.device)
+//        if (connection == null) {
+//            Toast.makeText(applicationContext, "can't open device", Toast.LENGTH_SHORT).show()
+//            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+//            return
+//        }
+//
+//        port = driver.ports[0] // Most devices have just one port (port 0)
+//
+//        port.open(connection)
+//        port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+//
+//        SerialInputOutputManager(port, this).start()
+////        Toast.makeText(applicationContext, "opened port POGCHAMP", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun sendbs(v: View) {
+//        if (::port.isInitialized) {
+//            if (port.isOpen) {
+//                try {
+//                    val edittext = findViewById<EditText>(R.id.edittext)
+//                    port.write((edittext.text.toString()+"\n").encodeToByteArray(), WRITE_WAIT_MILLIS)
+//                } catch (e: SerialTimeoutException) {
+//                    Toast.makeText(applicationContext, "SerialTimeoutException: $e", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                return
+//            }
+//        }
+//        Toast.makeText(applicationContext, "port not open", Toast.LENGTH_SHORT).show()
+//    }
 
     fun settings(v: View) {
 
@@ -105,26 +100,26 @@ class MainActivity : AppCompatActivity(), SerialInputOutputManager.Listener {
 //        }
 //        Toast.makeText(applicationContext, "port not open", Toast.LENGTH_SHORT).show()
     }
-
-    override fun onNewData(data: ByteArray?) {
-        var text: String = data?.let { String(it) }.toString();
-        this.runOnUiThread { Toast.makeText(this.applicationContext, "\"${text}\"", Toast.LENGTH_SHORT).show() }
-        var listview = findViewById<LinearLayout>(R.id.listview)
-        if (listview.children.lastOrNull() != null && text.last() != '\n') {
-            var entry = listview.children.last() as TextView;
-            entry.text = "${entry.text}${text}";
-        } else {
-            var entry = TextView(applicationContext)
-            entry.text = data?.let { String(it) }
-            this.runOnUiThread { listview.addView(entry) }
-        }
-
-    }
-
-    override fun onRunError(e: Exception?) {
-        this.runOnUiThread { Toast.makeText(this.applicationContext, "onRunError: $e", Toast.LENGTH_SHORT).show() }
-
-    }
+//
+//    override fun onNewData(data: ByteArray?) {
+//        var text: String = data?.let { String(it) }.toString();
+//        this.runOnUiThread { Toast.makeText(this.applicationContext, "\"${text}\"", Toast.LENGTH_SHORT).show() }
+//        var listview = findViewById<LinearLayout>(R.id.listview)
+//        if (listview.children.lastOrNull() != null && text.last() != '\n') {
+//            var entry = listview.children.last() as TextView;
+//            entry.text = "${entry.text}${text}";
+//        } else {
+//            var entry = TextView(applicationContext)
+//            entry.text = data?.let { String(it) }
+//            this.runOnUiThread { listview.addView(entry) }
+//        }
+//
+//    }
+//
+//    override fun onRunError(e: Exception?) {
+//        this.runOnUiThread { Toast.makeText(this.applicationContext, "onRunError: $e", Toast.LENGTH_SHORT).show() }
+//
+//    }
 
 
 }
